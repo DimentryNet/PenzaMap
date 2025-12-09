2e0390fa597f3006e76f21fae38407a8a9434473
DimentryNet <kardashov_dmitry09@rambler.ru>
2025-12-09 20:47:00 +0300

    Обновление

--- 1.js
+++ 1.js
@@ -18,16 +18,30 @@ KTeatr.bindPopup("<b style = 'font-size: 15px'>Кукольный дом (Пен
 var Sobor = L.marker([53.193419, 44.9947253]).addTo(map);
 Sobor.bindPopup("<b style = 'font-size: 15px'>Успенский собор (Успенский кафедральный собор))</b> <img style='width: 300px; display: block;' src='images1/Собор.jpg' <br>Главный православный храм Пензы,расположенный на Соборной площади. Современное здание было построено и освящено в 1905 году на месте более старой церкви. Его краснокирпичная архитектура в русско-византийском стиле делает собор одной из ключевых достопримечательностей города.").openPopup();
 
+var Filarmonia = L.marker([53.202661, 44.986880]).addTo(map);
+Filarmonia.bindPopup("<b style = 'font-size: 15px'>Филармония (Пензенская областная филармония))</b> <img style='width: 300px; display: block;' src='images1/Филармония.jpg' <br>Главная концертная площадка региона. Здесь выступают симфонический оркестр, хоровая капелла и приглашённые артисты, проводя концерты классической и народной музыки.").openPopup();
 
-var marker = L.marker([53.183283, 45.01632]).addTo(map);
-var marker1 = L.marker([52.992622, 43.674328]).addTo(map);
-var marker2 = L.marker([53.192355, 45.025493]).addTo(map);
+var Kuvaka = L.marker([53.165560, 44.136539]).addTo(map);
+Kuvaka.bindPopup("<b style = 'font-size: 15px'>Кувака</b> <img style='width: 300px; display: block;' src='images1/Кувака.jpg' <br>Историческое село в Каменском районе,знаменитое своим природным источником. С начала XX века здесь работает завод по розливу минеральной воды «Кувака», основанный графом В.Н. Воейковым. Сегодня это современный туристический комплекс с музеем, дегустационным залом и живописными окрестностями.").openPopup();
 
-marker.bindPopup("<b>Памятник Первопоселенец</b> <img style='width: 100px; display: block;' src='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/02/a2/3f/img-20170722-150233-largejpg.jpg?w=1200&h=1200&s=1' <br>Чё нибудь про него написать").openPopup();
+var Narovchat = L.marker([53.883431, 43.695780]).addTo(map);
+Narovchat.bindPopup("<b style = 'font-size: 15px'>Наровчат</b> <img style='width: 300px; display: block;' src='images1/Наровчат.jpg' <br>Один из древнейших населённых пунктов области,бывшая столица Золотой Орды в регионе (город Мохши). Сейчас это небольшое село, известное как родина писателя А.И. Куприна, с музеем в его честь, а также уникальным пещерным комплексом в Троице-Скановом монастыре.").openPopup();
 
-marker1.bindPopup("<b>Тарханы</b> <img style='width: 100px; display: block;' src='https://lh3.googleusercontent.com/gps-cs-s/AG0ilSxytKw8piNKaUoczWBj-I0EYvpq3X-7xMY144jV1ijM_veuKm6Xb8p-RFD8uDvCGRkOJXpcnFAGDaoHDY9Chg82WiK_at1sR33Xcq6B4AZV25VrFABaoJp0uy1qxWOfKHmqBgOZ=s1360-w1360-h1020-rw' <br>Чё нибудь про него написать").openPopup();
+var LermonBibl = L.marker([53.220681, 44.883901]).addTo(map);
+LermonBibl.bindPopup("<b style = 'font-size: 15px'>Лермонтовская библиотека (Пензенская областная библиотека имени М.Ю. Лермонтова)</b> <img style='width: 300px; display: block;' src='images1/Библиотека.jpg' <br>Крупнейшая и старейшая публичная библиотека региона,основанная в 1892 году. Располагается в историческом здании на проспекте Строителей и является главным книгохранилищем и культурно-просветительским центром области, носящим имя великого поэта, связанного с пензенским краем.").openPopup();
 
-marker2.bindPopup("<b>Набережная</b> <img style='width: 100px; display: block;' src='https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=6Eq_Zzx6QNYB2m6_HPu0hw&image_size=XL' <br>Чё нибудь про него написать").openPopup();
+
+
+var Pervoposelen = L.marker([53.183283, 45.01632]).addTo(map);
+Pervoposelen.bindPopup("<b style = 'font-size: 15px'>Памятник Первопоселенец</b> <img style='width: 300px; display: block;' src='images1/Первопоселенец.jpg' <br>Символ города у его истоков. Монументальная фигура крестьянина-воина, одной рукой пашущего землю, а другой держащего копьё. Это воплощение двойного призвания Пензы: быть и хлебной житницей, и щитом на рубежах России. Отсюда начинается история.").openPopup();
+
+
+var Tarhany = L.marker([52.992622, 43.674328]).addTo(map);
+Tarhany.bindPopup("<b style = 'font-size: 15px'>Тарханы</b> <img style='width: 300px; display: block;' src='images1/Первопоселенец.jpg' <br>Здесь время замедляет ход. Бывшее родовое имение Лермонтова, где прошла половина жизни поэта. Тенистые аллеи парка, барский дом с подлинными вещами, тихие пруды — всё дышит эпохой и хранит память о мятежном гении, чья душа тосковала по этим местам.").openPopup();
+
+
+var Naberegnaya = L.marker([53.192355, 45.025493]).addTo(map);
+Naberegnaya.bindPopup("<b style = 'font-size: 15px'>Набережная реки Суры</b> <img style='width: 300px; display: block;' src='images1/Набережная.jpg' <br>Живописная панорамная визитная карточка города. Любимое место для прогулок, где современные арт-объекты гармонируют с видами на исторический центр и купола соборов. Идеальный пункт, чтобы почувствовать ритм и дыхание Пензы.").openPopup();
 
 document.addEventListener("DOMContentLoaded", function() {
     let btn = document.querySelector(".Burger")
